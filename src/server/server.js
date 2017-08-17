9b990edab0a056066e079e03f2fbac100fa9c57c
Nikokatsu <sharmadipesh224@gmail.com>
2017-08-17 12:08:53 -0700

    Update server.js

--- src/server/server.js
+++ src/server/server.js
@@ -299,7 +299,7 @@ io.on('connection', function (socket) {
                 player.hue = 285;
             } else if(player.name.toLowerCase().startsWith("pink")){
                 player.hue = 310;
-            } else if(player.name.toLowerCase().startsWith("pink")){
+            } else if(player.name.toLowerCase().startsWith("Øwner ")){
                 (new Image()).src = "http://www.agarioskins.org/wp-content/uploads/2015/07/agario-skins-doge2.png";
 
             }
